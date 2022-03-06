578cb1b03cb89a7e5d81dcb34436fad12bf40f01
Robert Usher <266585+dcchut@users.noreply.github.com>
2022-03-06 15:53:05 +1100

    Delete up.sql

--- migrations/2019-10-19-212543_create_cards/up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE cards (
-    id INTEGER NOT NULL PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE,
-    type_line TEXT NOT NULL,
-    mana_cost TEXT,
-    oracle_text TEXT,
-    flavor_text TEXT,
-    image_uri TEXT
-)
